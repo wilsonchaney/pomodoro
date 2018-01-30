59b41db7951d92e39980568d24113170f51496e4
Wilson Chaney <wilsongchaney@gmail.com>
2018-01-30 18:46:54 +0100

    add scripts, hopefully final

---
+++ install.sh
@@ -0,0 +1,10 @@
+# repo path
+dir="`dirname \"$(readlink -f \"$0\")\"`"
+
+# add run executable permissions
+chmod +x ./start_pomo
+chmod +x ./cancel_pomo
+
+# Set up symlinks to start_pomo and kill_pomo
+sudo ln -s "$dir/start_pomo" /usr/bin/start_pomo
+sudo ln -s "$dir/cancel_pomo" /usr/bin/cancel_pomo
--- kill_pomo.sh
+++
@@ -1,3 +0,0 @@
-PROCESS_NAME=start_pomo
-echo "Killing any pomodoro instances - assuming name is '$PROCESS_NAME'"
-killall -s SIGINT $PROCESS_NAME
